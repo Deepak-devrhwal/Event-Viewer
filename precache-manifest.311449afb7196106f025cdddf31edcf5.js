769eb633058c1f7f2dedb44a012aaf310b4977ad
Deepak Devrhwal <deepakdevrhwal@gmail.com>
2020-08-27 21:12:01 +0530

    Updates

---
+++ precache-manifest.311449afb7196106f025cdddf31edcf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc3540e7ce61f475e331a4d65e54888",
+    "url": "/Event-Viewer/index.html"
+  },
+  {
+    "revision": "b11e06a37133c7200d13",
+    "url": "/Event-Viewer/static/css/main.bfd39287.chunk.css"
+  },
+  {
+    "revision": "0f4fd25eae0074faa1a3",
+    "url": "/Event-Viewer/static/js/2.63204b22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Event-Viewer/static/js/2.63204b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11e06a37133c7200d13",
+    "url": "/Event-Viewer/static/js/main.4cd8f244.chunk.js"
+  },
+  {
+    "revision": "27b7e1a0ce7c515d19dd",
+    "url": "/Event-Viewer/static/js/runtime-main.1ae1164c.js"
+  },
+  {
+    "revision": "ee331d1177a37d31fe5f29bb3d3c0df8",
+    "url": "/Event-Viewer/static/media/close.ee331d11.svg"
+  }
+]);
